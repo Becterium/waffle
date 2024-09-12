8c6f4758643858a7b04d85d0517df7408e3ab394
Becterium <1446555938@qq.com>
2024-09-12 21:50:55 +0800

    Add user business code at data

--- internal/data/user.go
+++ internal/data/user.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"waffle/internal/biz"
 )
@@ -19,22 +20,43 @@ func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
 }
 
 func (u *userRepo) CreateUser(ctx context.Context, user *biz.User) (*biz.User, error) {
-	return &biz.User{
-		Id:       0,
-		Username: "nani",
-		Password: "bsdnaklj",
-	}, nil
+	result := u.data.db.Create(user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user, nil
 }
 func (u *userRepo) GetUser(ctx context.Context, id int64) (*biz.User, error) {
-	return &biz.User{
-		Id:       0,
-		Username: "heheh",
-		Password: "andjka",
-	}, nil
+	user := &biz.User{
+		Id: id,
+	}
+	result := u.data.db.Find(user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
 }
 func (u *userRepo) VerifyPassword(ctx context.Context, user *biz.User) (bool, error) {
-	return false, nil
+	usertar := &biz.User{
+		Username: user.Username,
+	}
+	result := u.data.db.First(usertar)
+	if result.Error != nil {
+		return false, result.Error
+	}
+	if usertar.Password != user.Password {
+		return false, errors.New("mistake password")
+	}
+	return true, nil
 }
 func (u *userRepo) FindByUsername(ctx context.Context, username string) (*biz.User, error) {
-	return nil, nil
+	user := &biz.User{
+		Username: username,
+	}
+	result := u.data.db.First(user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
 }
